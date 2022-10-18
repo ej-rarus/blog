f30827f2f8ef2ccec4cc6a49de88dc38ef1a3544
ej-rarus <lpl2001@naver.com>
2022-10-18 17:20:08 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 /* eslint-disable */
 
 import './App.css';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 
 import Navibar from './components/Navibar.js';
+
 
 function App() {
   return (
     <div className="App">
+
+
       <Navibar></Navibar>
+
+      <BrowserRouter>
+        <Routes>
+          <Route>
+
+          </Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
+
+
   );
 }
 
--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,35 +1,28 @@
 /* eslint-disable */
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function Navibar() {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">LEE EUN JAE</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <div>
+            <div className="Navigate">
+                <div className="Logo">LEE EUN JAE</div>
+                <div className="Menuwrap">
+                    <div className="Nav-menu">ABOUT</div>
+                    <div className="Nav-menu">PHILOSOPHY</div>
+                    <div className="Nav-menu">DEV</div>
+                    <div className="Nav-menu">ART</div>
+                    <div className="Nav-menu">SPORT</div>
+                    <div className="Nav-menu">CONTACT</div>
+
+                </div>
+            </div>
+        </div>
+
     );
 }
 
