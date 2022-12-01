57ec5b8b0adda4591ee2cf8b53f88545ae9fc1a1
ej-rarus <lpl2001@naver.com>
2022-12-02 03:59:06 +0900

    main minor commit

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,5 +1,8 @@
 /* eslint-disable */
 import { useMediaQuery } from 'react-responsive';
+import { FaMapMarkerAlt, FaUser, FaCalendar, FaPhoneAlt, FaEnvelope, FaUserGraduate } from "react-icons/fa";
+
+
 
 function Mainpage() {
     const isDesktopOrLaptop = useMediaQuery({
@@ -19,29 +22,80 @@ function Mainpage() {
                         </div>
                         <div className="Main-card-content">
                             <div className="Main-card-name">Eunjae Tony Lee</div>
-                            <div className="Main-card-text">안녕</div>
+                            <div className="Main-card-text"> - 웹 프론트엔드 & ML 개발자</div>
+                            <div className="Main-card-badge-wrapper">
+                                <div className='Main-card-badge-shelf'>
+                                    <div className='Main-card-badge'>
+                                        <div className='Badge-icon'><FaUser /></div>
+                                        <div className='Badge-content'>
+                                            <div className='Badge-title'>이름</div>
+                                            <div className='Badge-text'>이은재</div>
+                                        </div>
+                                    </div>
+                                    <div className='Main-card-badge'>
+                                        <div className='Badge-icon'><FaMapMarkerAlt /></div>
+                                        <div className='Badge-content'>
+                                            <div className='Badge-title'>주소지</div>
+                                            <div className='Badge-text'>서울특별시 동작구</div>
+                                        </div>
+                                    </div>
+                                    <div className='Main-card-badge'>
+                                        <div className='Badge-icon'><FaCalendar /></div>
+                                        <div className='Badge-content'>
+                                            <div className='Badge-title'>생년월일</div>
+                                            <div className='Badge-text'>93.02.26</div>
+                                        </div>
+                                    </div>                                </div>
+                                <div className='Main-card-badge-shelf'>
+                                    <div className='Main-card-badge'>
+                                        <div className='Badge-icon'><FaPhoneAlt /></div>
+                                        <div className='Badge-content'>
+                                            <div className='Badge-title'>연락처</div>
+                                            <div className='Badge-text'>010-6289-6443</div>
+                                        </div>
+                                    </div>
+                                    <div className='Main-card-badge'>
+                                        <div className='Badge-icon'><FaEnvelope /></div>
+                                        <div className='Badge-content'>
+                                            <div className='Badge-title'>이메일</div>
+                                            <div className='Badge-text'>lpl2001@naver.com</div>
+                                        </div>
+                                    </div>
+                                    <div className='Main-card-badge'>
+                                        <div className='Badge-icon'><FaUserGraduate /></div>
+                                        <div className='Badge-content'>
+                                            <div className='Badge-title'>학력</div>
+                                            <div className='Badge-text'>중앙대학교</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
 
                     </div>
                 </div>
-            )}
-            {isTabletOrMobile && <div className="Mainpage-wrapper">
-                <div className="Main-card">
-                    <div className="Main-card-img-container-mobile">
-                        <img className="Main-card-img-mobile" src="img/main/profile.png">
+            )
+            }
+            {
+                isTabletOrMobile && <div className="Mainpage-wrapper">
+                    <div className="Main-card">
+                        <div className="Main-card-img-container-mobile">
+                            <img className="Main-card-img-mobile" src="img/main/profile.png">
 
-                        </img>
-                    </div>
-                    <div className="Main-card-content">
-                        <div className="Main-card-name-mobile">Eunjae Tony Lee</div>
-                        <div className="Main-card-text-mobile">안녕</div>
-                    </div>
+                            </img>
+                        </div>
+                        <div className="Main-card-content">
+                            <div className="Main-card-name-mobile">Eunjae Tony Lee</div>
+                            <div className="Main-card-text-mobile"> - 웹 프론트엔드 & ML 개발자</div>
+                        </div>
 
+                    </div>
                 </div>
-            </div>}
+            }
 
 
-        </div>
+        </div >
     )
 }
 
