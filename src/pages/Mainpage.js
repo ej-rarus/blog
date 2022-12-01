b8338a71f91578c2f74e3b787d6b3717f80589f2
ej-rarus <lpl2001@naver.com>
2022-12-01 17:44:03 +0900

    main name card update

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,9 +1,35 @@
 /* eslint-disable */
+import { useMediaQuery } from 'react-responsive';
 
 function Mainpage() {
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 1224px)'
+    })
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
+
     return (
         <div>
-            메인페이지입니다.
+            {isDesktopOrLaptop && (
+                <div className="Mainpage-wrapper">
+                    <div className="Main-card">
+                        <div className="Main-card-img-container">
+                            <img className="Main-card-img" src="img/main/profile.png">
+
+                            </img>
+                        </div>
+                        <div className="Main-card-content">
+                            <div className="Main-card-name">Eunjae Tony Lee</div>
+                            <div className="Main-card-text">
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            )}
+            {isTabletOrMobile && <div></div>}
+
+
         </div>
     )
 }
